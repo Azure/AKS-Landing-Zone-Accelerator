7dfbc0e5e4e23bf7a3a6a793e1fb8673c6b08a96
Ayobami Ayodeji <aayodeji@microsoft.com>
2021-08-31 00:13:25 -0400

    Update provider.tf

--- Scenarios/Secure-Baseline/Terraform/04-Network-Hub/provider.tf
+++ Scenarios/Secure-Baseline/Terraform/04-Network-Hub/provider.tf
@@ -10,7 +10,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate"      # Update this value
     storage_account_name = "tfstate-sa"   # Update this value
-    container_name       = "akscs"  
+    container_name       = "akscs"        # Update this value
     key                  = "hub-net"
   }
 
