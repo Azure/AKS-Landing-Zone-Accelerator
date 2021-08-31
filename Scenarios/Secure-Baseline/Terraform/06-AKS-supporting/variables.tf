e5e574527ea74d59abb23d549afa7ab06ef16103
Ayobami Ayodeji <aayodeji@microsoft.com>
2021-08-31 00:19:27 -0400

    Update variables.tf

--- Scenarios/Secure-Baseline/Terraform/06-AKS-supporting/variables.tf
+++ Scenarios/Secure-Baseline/Terraform/06-AKS-supporting/variables.tf
@@ -5,14 +5,14 @@
 variable "access_key" {}   # Provide using a .tfvars file. 
 
 variable "state_sa_name" {
-    default = "tfstatestorejkc"   # Update this value
+    default = "tfstate-sa"   # Update this value
 }
 
 variable "container_name" {
-    default = "escsjkc"    # Update this value
+    default = "akscs"    # Update this value
 }
 
 variable "prefix" {
-  default = "jkc"
+  default = "akscs"
 }
 
