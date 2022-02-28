e37f56bbe0e93ef6240506b4eee45057e7d42efe
Yassine EL GHALI <yelghali@gmail.com>
2022-02-28 20:15:58 +0100

    Update 01-aks1.tf

--- Scenarios/Backup-Restore/velero_terraform_sample/01-aks1.tf
+++ Scenarios/Backup-Restore/velero_terraform_sample/01-aks1.tf
@@ -54,20 +54,3 @@ resource "azurerm_role_assignment" "aks_mc_rg" {
 }
 
 
-
-#module "prepkube" {
-#  source = "./modules/aadpodid"
-#
-#  providers = {
-#    kubernetes = kubernetes.aks-module
-#    helm       = helm.aks-module
-#  }
-#
-#  aks_resource_group_name = azurerm_kubernetes_cluster.aks.node_resource_group
-#  location                = azurerm_kubernetes_cluster.aks.location
-#
-#  aadpodidentity_chart_version    = var.aadpodidentity_chart_version
-#  aadpodidentity_chart_repository = var.aadpodidentity_chart_repository
-#  aadpodidentity_namespace        = var.aadpodidentity_namespace
-#  aadpodidentity_values           = var.aadpodidentity_values
-#}
