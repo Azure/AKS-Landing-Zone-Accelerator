98a0b86e342ec0c282761802b7e5ac3361ca44f8
Houssem Dellai <hodellai@microsoft.com>
2023-04-03 11:34:13 +0200

    aks prom grafana with terraform

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/03-AAD-create/provider.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/03-AAD-create/provider.tf
@@ -5,7 +5,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "= 3.9.0"
+      version = "= 3.44.1"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "= 2.34.1"
     }
   }
 
