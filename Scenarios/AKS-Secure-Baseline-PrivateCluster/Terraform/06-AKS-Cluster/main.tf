6581834eb0905f0bc51533eff5ea10acc1ac72be
Houssem Dellai <houssem.dellai@live.com>
2024-10-28 09:55:37 +0000

    impl all in one TF

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/06-AKS-Cluster/main.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/06-AKS-Cluster/main.tf
@@ -118,6 +118,7 @@ resource "azurerm_kubernetes_cluster" "aks-cluster" {
   }
   azure_active_directory_role_based_access_control {
     # managed                = true
+    azure_rbac_enabled     = true
     admin_group_object_ids = [var.adminGroupObjectIds]
   }
 
