397f5a73b6a6150a04b77c7c724d992d766fa825
Houssem Dellai <houssem.dellai@live.com>
2024-11-12 10:30:50 +0000

    work in progress for branch tf-aks-backup

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/06-AKS-Cluster/output.tf
+++
@@ -1,7 +0,0 @@
-output "aksClusterId" {
-    value = azurerm_kubernetes_cluster.aks-cluster.id
-}
-
-output "aksClusterIdentityPrincipalId" {
-    value = azurerm_kubernetes_cluster.aks-cluster.identity[0].principal_id  
-}
